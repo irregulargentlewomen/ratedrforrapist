359eaabc851c40cefa16752954d1acc5f990fa84
Betsy Haibel <betsy.haibel@gmail.com>
2013-01-12 16:13:35 -0500

    adding length test to search results

--- spec/search_results_spec.rb
+++ spec/search_results_spec.rb
@@ -2,26 +2,37 @@ require_relative '../lib/search_results'
 require_relative 'spec_helper'
 
 describe SearchResults do
+  subject(:results) {SearchResults.get('title')}
+  let(:response) {
+    OpenStruct.new(code: 200, body: {
+      results: [
+        {
+          title: 'Fight Club',
+          id: 550,
+          release_date: '1999-10-15'
+        }
+      ]
+    }.to_json)
+  }
+  before do
+    HTTParty.stub(:get).
+      with("http://api.omdb.org/3/search/movie?query=title&api_key=key").
+      and_return(response)
+  end
+
   describe ".get" do
     context 'when there are results for a given title' do
-      before do
-        HTTParty.stub(:get).
-          with("http://api.omdb.org/3/search/movie?query=title&api_key=key").
-          and_return(OpenStruct.new(code: 200, body: {
-            results: [
-              {
-                title: 'Fight Club',
-                id: 550,
-                release_date: '1999-10-15'
-              }
-            ]
-          }.to_json))
-      end
       it 'returns a results object' do
         results = SearchResults.get('title')
         results.first.title.should == 'Fight Club'
         results.first.id.should == 550
       end
+    end
+  end
+
+  describe '#length' do
+    it 'returns the length of the result array' do
+      results.length.should == 1
     end
   end
 end
