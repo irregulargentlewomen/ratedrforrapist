c1141e3f946f6efc2aba5907256a315ceffd817e
Betsy Haibel <betsy.haibel@gmail.com>
2013-01-14 18:31:36 -0500

    adding ability to search a person's credits

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+require 'api_handler'
+
+class Person
+  attr_reader :id
+  attr_writer :api_key
+  def initialize(id)
+    @id = id
+  end
+
+  def movies
+    @movies ||= get_movies
+  end
+
+  private
+  include ApiHandler
+  
+  def get_movies
+    response_body = get_unless_down
+    response_body['cast'] + response_body['crew']
+  end
+
+  def url
+    "http://api.omdb.org/3/person/#{id}/credits?api_key=#{api_key}"
+  end
+end
---
+++ spec/lib/person_spec.rb
@@ -0,0 +1,58 @@
+require_relative '../../lib/person'
+require_relative '../spec_helper'
+
+describe Person do
+  let(:person) { Person.new(0) }
+  before do
+    person.api_key = "key"
+  end
+
+  it 'sets id on initialization' do
+    person.id.should == 0
+  end
+
+  describe "#movies" do
+    describe "when the API is available" do
+      before do
+        HTTParty.stub(:get).
+        with("http://api.omdb.org/3/person/0/credits?api_key=key").
+        and_return(OpenStruct.new(code: 200, body: {
+          id: 0,
+          cast: [
+            {
+              id: 107,
+              title: "Snatch"
+            }
+          ],
+          crew: [
+            {
+              id: 161,
+              title: "Ocean's Eleven"
+            }
+          ]
+        }.to_json))
+      end
+
+      it 'returns the correct result' do
+        result = person.movies
+        result.length.should == 2
+        result.select {|x| x['id'] == 107 && x['title'] == "Snatch"}.length.should == 1
+        result.select {|x| x['id'] == 161 && x['title'] == "Ocean's Eleven"}.length.should == 1   
+      end
+    end
+
+    describe "when the API is not available" do
+      before do 
+        HTTParty.stub(:get).and_return(OpenStruct.new(code: 500))
+      end
+
+      it 'raises an error' do
+        person.cast_and_crew.should raise_error
+      end
+
+      it 'tries 5 times before giving up' do
+        HTTParty.should_receive(:get).exactly(5).times
+      end
+    end
+  end
+end
