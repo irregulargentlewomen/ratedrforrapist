809dd64ac95871412c2cb3d637daed926cb54749
Betsy Haibel <betsy.haibel@gmail.com>
2013-04-05 21:48:11 -0400

    fleshing out push and pop tests

--- spec/javascripts/IrregularGentlewomenSpec.js
+++ spec/javascripts/IrregularGentlewomenSpec.js
@@ -1,13 +1,36 @@
 describe("IrregularGentlewomen", function() {
   beforeEach(function(){
-    $ = jasmine.createSpy('jquery').
-      andReturn(jasmine.createSpyObj('node', ['removeClass', 'addClass']));
+    node = jasmine.createSpyObj('node', ['removeClass', 'addClass']);
+    $ = jasmine.createSpy('jquery').andReturn(node);
   });
   describe('#pushPageState', function(){
-    it('adds the page state to the end of the array', function() {
+    beforeEach(function(){
       IrregularGentlewomen.pageStates = [''];
       IrregularGentlewomen.pushPageState('new-state');
+    });
+    it('adds the page state to the end of the array', function() {
       expect(IrregularGentlewomen.pageStates).toEqual(['', 'new-state']);
-    }); 
+    });
+    it('removes the old page state from the body class', function() {
+      expect(node.removeClass).toHaveBeenCalledWith('');
+    });
+    it('adds the new page state to the body class', function() {
+      expect(node.addClass).toHaveBeenCalledWith('new-state');
+    });
+  });
+  describe('#popPageState', function(){
+    beforeEach(function(){
+      IrregularGentlewomen.pageStates = ['', 'old-state'];
+      IrregularGentlewomen.popPageState();
+    });
+    it('removes the page state from the end of the array', function() {
+      expect(IrregularGentlewomen.pageStates).toEqual(['']);
+    });
+    it('removes the old page state from the body class', function() {
+      expect(node.removeClass).toHaveBeenCalledWith('old-state');
+    });
+    it('adds the new page state to the body class', function() {
+      expect(node.addClass).toHaveBeenCalledWith('');
+    });
   });
 });
